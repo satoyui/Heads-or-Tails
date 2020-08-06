25da35f5505740d1cf6354b2f78a22f94972a5b9
[s1270010] <[ys916@iCloud.com]>
2020-08-06 19:53:08 +0900

    HoT: Initial revision

---
+++ coin.java
@@ -0,0 +1,32 @@
+//import java.util.Scanner;
+public class coin{
+    public static void main(String[] args){
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Who are you?");
+        //String st = scanner.next();
+        //System.out.println("Hello, "+st+"!");
+        System.out.println("Tossing a coin");
+        int a = 0;
+        int b = 0;
+        for( int i=0 ; i<3 ; i++ ){
+            int n = 1+(int)(Math.random()*2);
+            int k = i+1;
+            if( n==1 ){
+                System.out.println("Round "+k+" : Heads");
+                a++;
+            }
+            else if( n==2 ){
+                System.out.println("Round "+k+" : Tails");
+                b++;
+            }
+        }
+        System.out.println("Heads: "+a+", Tails: "+b);
+        //if( a>b ){
+        //    System.out.println(st+" won!");
+        //}
+        //else{
+        //    System.out.println(st+" lost.");
+        //}
+
+    }
+}
